8c4acf10e89c0ff46670e6d19403fead8624cbb0
myliao <1739951529@qq.com>
2023-07-07 08:41:38 +0800

    完善好基本样式和根据参数样式下拉列表

--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -2,32 +2,43 @@
   <div class="hj_final_all">
     <div class="hj_head_ppt">
       <div class="hj_head_ppt_filter_div">
-        <!-- array_top: {{ array_top }} -->
-        <div class="hj_ppt_inline ">
-          <span class="hj_head_ppt_filter_div_name common_hj_font_style">功能领域:&nbsp;</span>
-          <el-select v-if="array_top.indexOf('spfd') !== -1" v-model="value_spfd" placeholder="请选择spfd"
-            @change="(value: any) => handleBuplOrSpfdChange(value, 'spfd')">
-            <el-option v-for="item in spfd_array" :key="item.pk" :label="item.name" :value="item.pk">
+        <!-- array_top: {{ array_top }} 侧边栏顶部部分 -->
+        <div class="hj_ppt_inline " v-if="d_id !== ''">
+          <span class="hj_head_ppt_filter_div_name common_hj_font_style">部门:&nbsp;</span>
+          <el-select filterable   v-model="value_dep" placeholder="请选择部门"
+            @change="(value: any) => handleBuplOrSpfdChange(value, 'd')">
+            <el-option v-for="item in dep_array" :key="item.pk" :label="item.name" :value="item.pk">
             </el-option>
           </el-select>
         </div>
-        <div class="hj_ppt_inline " v-if="array_top.indexOf('bupl') !== -1">
+
+        <div class="hj_ppt_inline " v-if="bupl_id !== ''">
           <span class="hj_head_ppt_filter_div_name common_hj_font_style">BUPL:&nbsp;</span>
-          <el-select v-model="value_bupl" placeholder="请选择pl"
+          <el-select filterable   v-model="value_bupl" placeholder="请选择pl"
             @change="(value: any) => handleBuplOrSpfdChange(value, 'bupl')">
             <el-option v-for="item in bupl_array" :key="item.pk" :label="item.name" :value="item.pk">
             </el-option>
           </el-select>
         </div>
 
-        <div class="hj_ppt_inline " v-if="array_top.indexOf('year') !== -1">
+        <div class="hj_ppt_inline " v-if="spfd_id !== ''">
+          <span class="hj_head_ppt_filter_div_name common_hj_font_style">功能领域:&nbsp;</span>
+          <el-select filterable   v-model="value_spfd" placeholder="请选择spfd"
+            @change="(value: any) => handleBuplOrSpfdChange(value, 'spfd')">
+            <el-option v-for="item in spfd_array" :key="item.pk" :label="item.name" :value="item.pk">
+            </el-option>
+          </el-select>
+        </div>
+
+        <div class="hj_ppt_inline " v-if="year_id !== ''">
           <span class="hj_head_ppt_filter_div_name common_hj_font_style">年份:&nbsp;</span>
           <el-select v-model="value_year" class="hj_head_ppt_filter_div_select" placeholder="请选择编制年份">
             <el-option v-for="item in year_array" :key="item.value" :label="item.label" :value="item.value" />
           </el-select>
         </div>
+        
         <!-- 年份快速筛选 -->
-        <div v-if="array_top.indexOf('year') !== -1" class="hj_ppt_inline fast_year_font fast_year_style">
+        <div v-if="year_id !== ''" class="hj_ppt_inline fast_year_font fast_year_style">
           <a v-for="( item, index ) in  my_funture_years " class=" hj_ppt_a " :key="item" @click="selectYear(item)"><span
               class="higet_ppt_pointer hj_ppt_font">{{ item }}</span><span v-if="index !== my_funture_years.length - 1"
               class="hj_ppt_font">&nbsp;|&nbsp;</span></a>
@@ -46,7 +57,7 @@
           <div class="hj_right_adjust_icon"></div>
         </div>
 
-        <!-- 公司： {{ company_array }} -->
+        <!-- 公司： {{ company_array }} 侧边栏部分 -->
         <div v-if="company_array.length > 0" class="hj_sidebar_category_ppt">
           <span class="higet_ppt_span_bold " style="font-size: 1.1em;">公司维度</span>
           <div class="hj_sidebar_ppt_item " :data-table_pk="company_array[0].pk" data-form_type="c">
@@ -58,7 +69,21 @@
           </div>
           <div v-if="bupl_array.length > 0 || spfd_array.length > 0" class="hj_slider_bottom_line"></div>
         </div>
-        <!-- bupl部门  -->
+
+        <!-- 部门 -->
+        <div v-if="company_array.length > 0" class="hj_sidebar_category_ppt">
+          <span class="higet_ppt_span_bold " style="font-size: 1.1em;">部门</span>
+          <div v-for="(item, index) in dep_array" class="hj_sidebar_ppt_item "  :key="index" :data-table_pk="item.pk"  data-form_type="d">
+            <div class="hj_sidebar_ppt_icon"></div>
+            <a class="higet_ppt_pointer hj_siderbar_ppt_height hj_ppt_font "
+            @click="hj_ppt_sidebar_select(item.pk ? item.pk : null, item.name ? item.name : null, 'd')">
+               {{ item.name }}
+            </a>
+          </div>
+          <div v-if="bupl_array.length > 0 || spfd_array.length > 0" class="hj_slider_bottom_line"></div>
+        </div>
+
+        <!-- bupl  -->
         <div v-if="bupl_array.length > 0" class="hj_sidebar_category_ppt">
           <span class="higet_ppt_span_bold " style="font-size: 1.1em;">BUPL</span>
           <div v-for="(item, index) in bupl_array" class="hj_sidebar_ppt_item " :key="index" :data-table_pk="item.pk"
@@ -144,9 +169,10 @@ export default defineComponent({
     usePasteEvent()
 
     // 定义数组来存储参数
-    // const pl_id: (string | null)[] = []
-    // const year: (string | null)[] = []
-    // const spfd_id: (string | null)[] = [] //  spfd 功能领域的问题
+    const bupl_id = ref('')
+    const year_id = ref('')
+    const spfd_id = ref('')  //  spfd 功能领域的问题
+    const d_id = ref('')  //  spfd 功能领域的问题
 
 
     // 处理链接和维度的问题 
@@ -155,58 +181,73 @@ export default defineComponent({
     // let array_top: any[] = []
     const array_left = ref<string[]>([])
     const array_top = ref<string[]>([])
+
+    // 根据参数渲染数据
     const handleUrlAndSleect = (url: string) => {
       // 创建一个新的URL对象
-      const urlObj = new URL(url, 'http://example.com')
-      // const params = new URLSearchParams(window.location.search)
-
+      // const urlObj = new URL(url, 'http://example.com')
+      const params = new URLSearchParams(window.location.search)
       // 使用URLSearchParams对象来获取查询参数
-      const params = new URLSearchParams(urlObj.search)
-      if (urlObj && urlObj.search) {
-        //  获取top_array 
-        if (params.has('array_top')) {
-          const arrayParam = params.get('array_top')
-          if (arrayParam) {
-            try {
-              array_top.value = JSON.parse(decodeURIComponent(arrayParam.replace(/'/g, '"')))
-              console.log(array_top, 'array_top')
-            } catch (e) {
-              ElMessage.error('参数解析错误')
-            }
-          } else {
-            ElMessage.error('头部至少要有一个下拉列表')
-          }
-          if (array_top.value.indexOf('year') !== -1) {
-            getYearData()
+      // const params = new URLSearchParams(urlObj.search)
+      console.log(params, "最终的params")
+
+      // if (urlObj && urlObj.search) {
+      if (params) {
+        // 左侧列表和上侧下拉列表一样  除了公司
+        // 接下来根据参数是否有获取数据信息
+
+        // 标记要显示的  填充当前选中的
+        if (params.has('spfd_id')) {
+          const temp = params.get('spfd_id')
+          if (temp && temp !== '-1' && temp !== '0') {
+            getSpfdData()
+            spfd_id.value = temp
+            value_spfd.value = Number(temp)
           }
         }
-        //  获取左侧的下拉列表
-        if (params.has('array_left')) {
-          //  将参数添加到对应的数组中
-          const arrayParam = params.get('array_left')
-          if (arrayParam) {
-            //  存储 头部的下拉列表
-            array_left.value = JSON.parse(decodeURIComponent(arrayParam.replace(/'/g, '"')))
-            // 接下来根据参数是否有获取数据信息
-            if (array_left.value.indexOf('company') !== -1) {
-              getCompanyData()
-            }
-            if (array_left.value.indexOf('spfd') !== -1) {
-              getSpfdData()
-            }
-            if (array_left.value.indexOf('bupl') !== -1) {
-              getBuplData()
-            }
-            console.log(array_left, 'array_left')
+        // bupl
+        if (params.has('bupl_id')) {
+          const temp = params.get('bupl_id')
+          if (temp && temp !== '-1' && temp !== '0') {
+            getBuplData()
+            bupl_id.value = temp
+            value_bupl.value = Number(temp)
           }
-          else {
-            ElMessage.error('左侧至少要有一个下拉列表')
+        }
+        // d 部门
+        if (params.has('d_id')) {
+          const temp = params.get('d_id')
+          if (temp) {
+            getDepData()
+            d_id.value = temp
+            value_dep.value = Number(temp)
+          }
+        }
+        // 年份
+        if (params.has('year')) {
+          const temp = params.get('year')
+          if (temp) {
+            getYearData()
+            year_id.value = temp
+            value_year.value = Number(temp)
+          }
+        } else {
+          ElMessage.error('缺少年份信息')
+        }
+
+        if (params.has('show_c')) {
+          // 如果其余3个皆为空时，公司也不用展示，因为就是公司
+          if (bupl_id.value !== '' || spfd_id.value !== '' || d_id.value !== '') {
+            getCompanyData()
           }
         }
 
         // 获取标题
         if (params.has('title')) {
           const arrayParam = params.get('title')
+          const arrayParam2 = params.get('d_id')
+          console.log(typeof arrayParam)
+          console.log(typeof arrayParam2)
           if (arrayParam) {
             title.value = arrayParam
           }
@@ -217,10 +258,15 @@ export default defineComponent({
       }
     }
     const value = ref('')
+    // 标记当前选了那个
     const value_year = ref<any>(new Date().getFullYear())
     const value_bupl = ref<any>(null)
     const value_spfd = ref<any>(null)
+    const value_dep = ref<any>(null)
+    const value_c = ref<any>(null)
+
     const company_array = ref<any[]>([]) // 公司维度
+    const dep_array = ref<any[]>([]) // 部门维度
     const bupl_array = ref<any[]>([]) // bupl 
     const spfd_array = ref<any[]>([]) // spfd功能领域
     const year_array = ref<any[]>([]) // 年份
@@ -246,8 +292,8 @@ export default defineComponent({
     // 获取几个维度的数据
     const getBuplData = () => {
       bupl_array.value = [
-        { pk: 300, name: "BUPL1" },
-        { pk: 400, name: "BUPL2" },
+        { pk: 1, name: "BUPL1" },
+        { pk: 2, name: "BUPL2" },
       ]
     }
 
@@ -280,8 +326,8 @@ export default defineComponent({
 
     const getSpfdData = () => {
       spfd_array.value = [
-        { pk: 100, name: "SPFD1" },
-        { pk: 2000, name: "SPFD2" },
+        { pk: 1, name: "SPFD1" },
+        { pk: 2, name: "SPFD2" },
       ]
     }
 
@@ -310,6 +356,14 @@ export default defineComponent({
       ]
     }
 
+    // 部门
+    const getDepData = () => {
+      dep_array.value = [
+        { pk: 1, name: "SPFD1" },
+        { pk: 2, name: "SPFD2" },
+      ]
+    }
+
     let selectedPk = ref<any>(null)
     // 切换选择的维度
     // 选中的样式
@@ -331,19 +385,31 @@ export default defineComponent({
             if (formType === 'bupl') {
               value_bupl.value = pk
               value_spfd.value = null
+              value_dep.value = null
+              value_c.value = null
             } else if (formType === 'spfd') {
               value_spfd.value = pk
+              value_bupl.value = null
+              value_dep.value = null
+              value_c.value = null
+            } else if (formType === 'd') {
+              value_dep.value = pk
+              value_spfd.value = null
+              value_bupl.value = null
+              value_c.value = null
+            } else if (formType === 'c') { 
+              value_c.value = pk
+              value_dep.value = null
+              value_spfd.value = null
               value_bupl.value = null
             }
             // 更新标题中的部门
             select_pk_name.value = pk_name
-
           }
         })
-      } else {
-        ElMessage.error('缺少对应pk值或formType，请联系管理员')
       }
     }
+
 
     // bupl选中状态
     const handleBuplOrSpfdChange = (val: any, name: string) => {
@@ -370,7 +436,12 @@ export default defineComponent({
       // alert('加载导入的数据中')
       // importSpecificData([])
       // handleUrlAndSleect('/primary_data/company_strategy_action_pl?pl_id=01&year=2023&array_top='['year:1','spfd:1','pl:1']')
-      handleUrlAndSleect('/primary_data/company_strategy_action_pl?pl_id=01&year=2023&title=年度人才计划&array_top=' + encodeURIComponent(JSON.stringify(['year', 'spfd', 'bupl'])) + '&array_left=' + encodeURIComponent(JSON.stringify(['company', 'spfd', 'bupl'])))
+      // 'href': '/primary_data/hz_ppt?bupl_id=1&spfd_id=0&d_id=1&year=2023&grid_key=ppt&show_c=1&title=ppt页面',
+      // handleUrlAndSleect('/primary_data/hz_ppt?bupl_id=1&spfd_id=1&d_id=1&year=2023&grid_key=ppt&show_c=1&title=ppt页面')
+      handleUrlAndSleect(url)
+
+
+      // handleUrlAndSleect('/primary_data/company_strategy_action_pl?pl_id=01&year=2023&title=年度人才计划&array_top=' + encodeURIComponent(JSON.stringify(['year', 'spfd', 'bupl'])) + '&array_left=' + encodeURIComponent(JSON.stringify(['company', 'spfd', 'bupl'])))
     })
 
     return {
@@ -382,22 +453,29 @@ export default defineComponent({
       pptData,
       handleUrlAndSleect,
       url,
-      value_year,
       my_funture_years,
       selectYear,
-      array_left,
-      array_top,
       title,
+      // 标记当前选了那个
       value_bupl,
       value_spfd,
+      value_dep,
+      value_year,
+      // 存储对应的下拉列表数据
       company_array,
       bupl_array,
       spfd_array,
       year_array,
+      dep_array,
       hj_ppt_sidebar_select,
       selectedPk,
       handleBuplOrSpfdChange,
       select_pk_name,
+      // 标记 要显示那些维度的下拉列表
+      bupl_id,
+      year_id,
+      spfd_id,
+      d_id,
     }
 
   },
